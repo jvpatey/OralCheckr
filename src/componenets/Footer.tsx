e20f3e9a2bca651985c6c5b95a5846759c1b79c6
Jeffrey Patey <jeffreyvpatey@gmail.com>
2024-06-18 22:30:15 -0300

    update footer component to use bootstrap container

--- src/componenets/Footer.tsx
+++ src/componenets/Footer.tsx
@@ -1,16 +1,10 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 export function Footer() {
   return (
     <footer className="footer">
       <Container className="footer">
-        <Row>
-          <Col>
-            <p>
-              &copy; 2024 OralCheckr - All Rights Reserved - GetCoding Module 3
-            </p>
-          </Col>
-        </Row>
+        <p>&copy; 2024 OralCheckr - All Rights Reserved - GetCoding Module 3</p>
       </Container>
     </footer>
   );
