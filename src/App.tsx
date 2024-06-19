c2e8a808f15d78289c6106390a197a8949a0425d
Rishab Gupta <rishabgupta98@gmail.com>
2024-06-19 08:20:00 -0300

    adds guarded route

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { StartQuestionnaire } from './pages/StartQuestionnaire';
 import { Questionnaire } from './pages/Questionnaire';
 import { Results } from './pages/Results';
 import { Recommendations } from './pages/Recommendations';
+import { GuardedRoute } from './containers/GuardedRoute';
 
 export function App() {
   const isAuthenticated = localStorage.getItem('authenticated') === 'true';
@@ -13,11 +14,19 @@ export function App() {
     <BrowserRouter>
       <Routes>
         {/* landing page at root */}
-        <Route path="/" element={<Landing />}></Route>{' '}
-        <Route path="/startQuestionnaire" element={isAuthenticated ? <StartQuestionnaire /> : <Navigate to="/" />} />
+        <Route path="/" element={<Landing />} />
+        <Route
+          path="/startQuestionnaire"
+          element={
+            <GuardedRoute>
+              <StartQuestionnaire />
+            </GuardedRoute>
+          }
+        />
         <Route path="/questionnaire" element={isAuthenticated ? <Questionnaire /> : <Navigate to="/" />} />
         <Route path="/results" element={isAuthenticated ? <Results /> : <Navigate to="/" />} />
         <Route path="/recommendations" element={isAuthenticated ? <Recommendations /> : <Navigate to="/" />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
