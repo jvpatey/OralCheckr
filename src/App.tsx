70dc003e2ea536862f9270106f929a1279120ad6
Jeffrey Patey <jeffreyvpatey@gmail.com>
2024-06-21 12:06:40 -0300

    added guarded routes for all routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import './App.css';
-import { Landing } from './pages/Landing';
-import { StartQuestionnaire } from './pages/StartQuestionnaire';
-import { Questionnaire } from './pages/Questionnaire';
-import { Results } from './pages/Results';
-import { Recommendations } from './pages/Recommendations';
-import { GuardedRoute } from './containers/GuardedRoute';
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import "./App.css";
+import { Landing } from "./pages/Landing";
+import { StartQuestionnaire } from "./pages/StartQuestionnaire";
+import { Questionnaire } from "./pages/Questionnaire";
+import { Results } from "./pages/Results";
+import { Recommendations } from "./pages/Recommendations";
+import { GuardedRoute } from "./containers/GuardedRoute";
 
 export function App() {
-  const isAuthenticated = localStorage.getItem('authenticated') === 'true';
-
   return (
     <BrowserRouter>
       <Routes>
-        {/* landing page at root */}
         <Route path="/" element={<Landing />} />
         <Route
           path="/startQuestionnaire"
@@ -23,9 +20,30 @@ export function App() {
             </GuardedRoute>
           }
         />
-        <Route path="/questionnaire" element={isAuthenticated ? <Questionnaire /> : <Navigate to="/" />} />
-        <Route path="/results" element={isAuthenticated ? <Results /> : <Navigate to="/" />} />
-        <Route path="/recommendations" element={isAuthenticated ? <Recommendations /> : <Navigate to="/" />} />
+        <Route
+          path="/questionnaire"
+          element={
+            <GuardedRoute>
+              <Questionnaire />
+            </GuardedRoute>
+          }
+        />
+        <Route
+          path="/results"
+          element={
+            <GuardedRoute>
+              <Results />
+            </GuardedRoute>
+          }
+        />
+        <Route
+          path="/recommendations"
+          element={
+            <GuardedRoute>
+              <Recommendations />
+            </GuardedRoute>
+          }
+        />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
