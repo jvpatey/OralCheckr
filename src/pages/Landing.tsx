8b0ba2af356b43dabc5d9fef1b22b81ce468c9e8
Jeffrey Patey <jeffreyvpatey@gmail.com>
2024-06-21 14:28:47 -0300

    moved button logic, and button, text, and title components into Landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,18 +1,41 @@
-import { Card } from 'react-bootstrap';
-import { LogoTitle } from '../componenets/LogoTitle';
-import { IntroText } from '../componenets/IntroText';
-import { StartButton } from '../componenets/StartButton';
-import { Footer } from '../componenets/Footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
+import { Footer } from "../components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export function Landing() {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    localStorage.setItem("authenticated", "true");
+    navigate("/startQuestionnaire");
+  };
+
   return (
     <div className="full-page-bg">
       <Card className="animated-card">
         <Card.Body className="card-style">
-          <LogoTitle />
-          <IntroText />
-          <StartButton />
+          <div className="logo-style">
+            <img
+              src="public/images/logo2.png"
+              alt="Logo"
+              style={{ height: "90px", marginBottom: "5px" }}
+            />
+            <span style={{ color: "#EFEFEF", fontSize: "50px" }}>
+              OralCheckr
+            </span>
+          </div>
+          <Card.Text className="text-style">
+            Welcome to OralCheckr, your personalized oral health checker app.
+          </Card.Text>
+          <Card.Text className="text-style">
+            Take our questionnaire to get your oral health status and
+            personalized recommendations.
+          </Card.Text>
+          <Button size="lg" className="landing-btn" onClick={handleLogin}>
+            Get Started
+          </Button>
         </Card.Body>
       </Card>
       <Footer />
