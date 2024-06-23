4ab188f77c8658afed5505f1df16685bec93033a
Jeffrey Patey <jeffreyvpatey@gmail.com>
2024-06-23 13:36:03 -0300

    added NavBar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,40 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+interface NavBarProps {
+  links: { text: string; href: string }[];
+}
+
+export function NavBar({ links }: NavBarProps) {
+  return (
+    <Navbar expand="lg" fixed="top" className="custom-navbar">
+      <Container fluid>
+        <Navbar.Brand href="#home" className="brand-text">
+          React-Bootstrap
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            className="bi bi-menu-button custom-icon"
+            viewBox="0 0 16 16"
+          >
+            <path d="M0 1.5A1.5 1.5 0 0 1 1.5 0h8A1.5 1.5 0 0 1 11 1.5v2A1.5 1.5 0 0 1 9.5 5h-8A1.5 1.5 0 0 1 0 3.5zM1.5 1a.5.5 0 0 0-.5.5v2a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 0-.5-.5z" />
+            <path d="m7.823 2.823-.396-.396A.25.25 0 0 1 7.604 2h.792a.25.25 0 0 1 .177.427l-.396.396a.25.25 0 0 1-.354 0M0 8a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm1 3v2a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2zm14-1V8a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2zM2 8.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0 4a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5" />
+          </svg>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav" className="custom-collapse">
+          <Nav className="ms-auto">
+            {links.map((link, index) => (
+              <Nav.Link key={index} href={link.href}>
+                {link.text}
+              </Nav.Link>
+            ))}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { NavBar } from "../components/NavBar";
+
 export function Dashboard() {
-  return <div>Dashboard</div>;
+  const links = [
+    { text: "Dashboard", href: "/dashboard" },
+    { text: "Questionnaire", href: "/questionnaire" },
+  ];
+
+  return (
+    <div className="full-page-bg">
+      <NavBar links={links} />
+    </div>
+  );
 }
