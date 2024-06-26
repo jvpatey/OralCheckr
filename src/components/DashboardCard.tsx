89ab8e0822cb95c7e094198aba06f7d69ebbd5b1
Rishab Gupta <rishabgupta98@gmail.com>
2024-06-26 09:04:01 -0400

    adds styled components

--- src/components/DashboardCard.tsx
+++ src/components/DashboardCard.tsx
@@ -1,15 +1,27 @@
 import { Card } from "react-bootstrap";
+import styled from "styled-components";
 
 interface DashboardCardProps {
   title: string;
 }
 
+const StyledCard = styled(Card)`
+  width: 80vw;
+  height: 100%;
+  min-width: 300px;
+  min-height: 400px;
+  background-color: #e0e0e0;
+  border: transparent;
+  border-radius: 20px;
+  background-color: green;
+`;
+
 export function DashboardCard({ title }: DashboardCardProps) {
   return (
-    <Card className="dashboard-card">
+    <StyledCard>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
       </Card.Body>
-    </Card>
+    </StyledCard>
   );
 }
