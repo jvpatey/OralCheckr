a97ef275d062f333a4580b5102ecaac06ea3e410
Rishab Gupta <rishabgupta98@gmail.com>
2024-06-26 19:27:06 -0400

    fixes css

--- src/components/DashboardCard.tsx
+++ src/components/DashboardCard.tsx
@@ -1,26 +1,15 @@
 import { Card } from "react-bootstrap";
-import styled from "styled-components";
 
 interface DashboardCardProps {
   title: string;
 }
 
-const StyledCard = styled(Card)`
-  // width: 80vw;
-  // height: 100%;
-  // min-width: 300px;
-  // min-height: 400px;
-  // background-color: #e0e0e0;
-  // border: transparent;
-  // border-radius: 20px;
-`;
-
 export function DashboardCard({ title }: DashboardCardProps) {
   return (
-    <StyledCard>
+    <Card>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
       </Card.Body>
-    </StyledCard>
+    </Card>
   );
 }
