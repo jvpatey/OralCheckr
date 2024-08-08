ec7631ba55f178b63e177659477721550fa9ce87
Jeffrey Patey <jeffreyvpatey@gmail.com>
2024-08-08 12:04:01 -0300

    added date bubbles and date picker to habits view

--- src/pages/HabitTracker/Habits.tsx
+++ src/pages/HabitTracker/Habits.tsx
@@ -1,7 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState, useEffect } from "react";
-import { Modal, Button, Form } from "react-bootstrap";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import styled from "styled-components";
+import { Modal, Button, Form } from "react-bootstrap";
 import _ from "lodash";
 import { PageBackground } from "../../components/styled/PageBackground";
 import { Sidebar } from "../../components/Sidebar";
@@ -10,6 +12,11 @@ import { HabitTile } from "../../components/habittracker/HabitTile";
 import { AddHabitTile } from "../../components/habittracker/AddHabitTile";
 import { StyledModal } from "../../components/styled/Modal";
 import { LogButton } from "../../components/habittracker/LogButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 // Styled component for the habit list container
 const HabitListContainer = styled.div`
@@ -54,6 +61,44 @@ const HeaderText = styled.div`
   color: #848889;
 `;
 
+// Styled component for the date controls
+const DateControlsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+// Styled component for the date bubbles container
+const DayBubbleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
+  margin-top: 30px;
+`;
+
+// Styled component for the date bubbles
+const DayBubble = styled.div<{ selected: boolean }>`
+  background-color: ${({ selected }) => (selected ? "#3f93b2" : "#ccc")};
+  color: white;
+  width: 50px;
+  height: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  margin: 0 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #3f93b2;
+    transform: scale(1.05);
+  }
+`;
+
+// Styled component for the list of entered habits
 const HabitList = styled.div`
   display: flex;
   flex-direction: column;
@@ -65,6 +110,7 @@ const HabitList = styled.div`
   overflow-x: hidden;
 `;
 
+// Styled component for row containing the habit tile and log button
 const HabitRow = styled.div`
   display: flex;
   align-items: center;
@@ -72,19 +118,85 @@ const HabitRow = styled.div`
   width: 100%;
 `;
 
+// Styled component for the date picker
+const CustomDatePickerInput = styled.input`
+  background-color: #f5f5f5;
+  border: none;
+  color: #3f93b2;
+  border: 2px solid #3f93b2;
+  padding: 8px 12px;
+  border-radius: 10px;
+  text-align: center;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #3f93b2;
+    color: #f5f5f5;
+  }
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 3px 2px rgba(53, 122, 150, 0.5);
+  }
+`;
+
+// Styled component for the arrow buttons
+const ArrowButton = styled.button`
+  background: none;
+  border: none;
+  color: #3f93b2;
+  font-size: 20px;
+  cursor: pointer;
+  border-radius: 50%;
+  width: 30px;
+  height: 45px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    background-color: #dfdfdf;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+// Helper function to get the start of the week for a given date
+const getStartOfWeek = (date: Date) => {
+  const startOfWeek = new Date(date);
+  startOfWeek.setDate(startOfWeek.getDate() - startOfWeek.getDay());
+  return startOfWeek;
+};
+
+// Helper function to get an array of the days in the week
+const getDaysInWeek = (startOfWeek: Date) => {
+  const days = [];
+  for (let i = 0; i < 7; i++) {
+    const day = new Date(startOfWeek);
+    day.setDate(startOfWeek.getDate() + i);
+    days.push(day);
+  }
+  return days;
+};
+
 // Types
 interface Habit {
   name: string;
   count: number;
 }
 
-// Functional component for Habits
 export function Habits() {
   // State to store the list of habits
   const [habits, setHabits] = useState<Habit[]>([]);
+  // State to store the currently selected date in the DatePicker
+  const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+  // State to store the currently selected day of the week
+  const [selectedDay, setSelectedDay] = useState<number>(new Date().getDay());
 
   // State to control the visibility of the modal
-  const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState<boolean>(false);
 
   // State to store the new habit being added or edited
   const [newHabit, setNewHabit] = useState<Habit>({
@@ -185,11 +297,74 @@ export function Habits() {
     (newHabit.name === originalHabit.name &&
       newHabit.count === originalHabit.count);
 
+  // Get the start of the week and the days in the week
+  const startOfWeek = getStartOfWeek(selectedDate);
+  const daysInWeek = getDaysInWeek(startOfWeek);
+
+  // Format the week range
+  const weekRange = `${startOfWeek.toLocaleDateString("en", {
+    month: "short",
+    day: "numeric",
+  })} - ${daysInWeek[6].toLocaleDateString("en", {
+    month: "short",
+    day: "numeric",
+  })}`;
+
+  // Handler functions for changing the selected date
+  const handlePrevWeek = () => {
+    setSelectedDate((prevDate) => {
+      const newDate = new Date(prevDate);
+      newDate.setDate(prevDate.getDate() - 7);
+      return newDate;
+    });
+  };
+
+  const handleNextWeek = () => {
+    setSelectedDate((prevDate) => {
+      const newDate = new Date(prevDate);
+      newDate.setDate(prevDate.getDate() + 7);
+      return newDate;
+    });
+  };
+
+  // Inside the return statement
   return (
     <PageBackground>
       <Sidebar links={links} />
       <HabitListContainer>
         <HabitWrapper>
+          <DateControlsContainer>
+            <DatePicker
+              selected={selectedDate}
+              onChange={(date: Date | null) => {
+                if (date) setSelectedDate(date);
+              }}
+              dateFormat="MMMM d, yyyy"
+              showWeekNumbers
+              customInput={<CustomDatePickerInput />}
+              placeholderText={weekRange}
+            />
+            <DayBubbleContainer>
+              <ArrowButton onClick={handlePrevWeek}>
+                <FontAwesomeIcon icon={faChevronLeft} />
+              </ArrowButton>
+              {daysInWeek.map((day, index) => (
+                <DayBubble
+                  key={index}
+                  selected={index === selectedDay}
+                  onClick={() => setSelectedDay(index)}
+                >
+                  <div>{day.getDate()}</div>
+                  <div>
+                    {day.toLocaleDateString("en", { weekday: "short" })[0]}
+                  </div>
+                </DayBubble>
+              ))}
+              <ArrowButton onClick={handleNextWeek}>
+                <FontAwesomeIcon icon={faChevronRight} />
+              </ArrowButton>
+            </DayBubbleContainer>
+          </DateControlsContainer>
           <Header>
             <HeaderText>My Habits:</HeaderText>
             <AddHabitTile onAddClick={handleAddHabitClick} />
