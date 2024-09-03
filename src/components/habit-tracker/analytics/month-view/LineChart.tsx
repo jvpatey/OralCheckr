946552d1c1095d8b850aa3d58dfb9b4cc9b596f6
Jeffrey Patey <jeffreyvpatey@gmail.com>
2024-09-02 22:19:55 -0300

    capitalized month names for linechart tooltips

--- src/components/habit-tracker/analytics/month-view/LineChart.tsx
+++ src/components/habit-tracker/analytics/month-view/LineChart.tsx
@@ -39,119 +39,126 @@ interface LineChartProps {
 // Function to generate the ApexCharts options object
 const generateChartOptions = (
   daysInMonth: number,
-  month: string
-): ApexCharts.ApexOptions => ({
-  chart: {
-    type: "line",
-    height: "100%",
-    toolbar: {
-      show: false,
-    },
-  },
-  stroke: {
-    curve: "smooth",
-    width: 2,
-  },
-  xaxis: {
-    categories: Array.from({ length: daysInMonth }, (_, i) =>
-      (i + 1).toString()
-    ),
-    title: {
-      text: "Day of the Month",
-      style: {
-        color: colors.green,
+  year: number,
+  monthIndex: number
+): ApexCharts.ApexOptions => {
+  const monthName = new Date(year, monthIndex).toLocaleString("default", {
+    month: "long",
+  });
+
+  return {
+    chart: {
+      type: "line",
+      height: "100%",
+      toolbar: {
+        show: false,
       },
     },
-    labels: {
-      style: {
-        colors: colors.blue,
-        fontSize: "10px",
-      },
-      rotate: -45,
-      hideOverlappingLabels: true,
+    stroke: {
+      curve: "smooth",
+      width: 2,
     },
-  },
-  yaxis: {
-    title: {
-      text: "Logs",
-      style: {
-        color: colors.green,
+    xaxis: {
+      categories: Array.from({ length: daysInMonth }, (_, i) =>
+        (i + 1).toString()
+      ),
+      title: {
+        text: "Day of the Month",
+        style: {
+          color: colors.green,
+        },
+      },
+      labels: {
+        style: {
+          colors: colors.blue,
+          fontSize: "10px",
+        },
+        rotate: -45,
+        hideOverlappingLabels: true,
       },
     },
-    labels: {
-      style: {
-        colors: colors.blue,
+    yaxis: {
+      title: {
+        text: "Logs",
+        style: {
+          color: colors.green,
+        },
       },
-      formatter: (value: number) => Math.floor(value).toString(),
+      labels: {
+        style: {
+          colors: colors.blue,
+        },
+        formatter: (value: number) => Math.floor(value).toString(),
+      },
+      min: 0,
+      tickAmount: 5,
     },
-    min: 0,
-    tickAmount: 5,
-  },
-  colors: [colors.blue],
-  markers: {
-    size: 4,
     colors: [colors.blue],
-    strokeColors: colors.white,
-    strokeWidth: 2,
-  },
-  tooltip: {
-    enabled: true,
-    x: {
-      formatter: (dayOfMonth: number) => `${month} ${dayOfMonth}`,
+    markers: {
+      size: 4,
+      colors: [colors.blue],
+      strokeColors: colors.white,
+      strokeWidth: 2,
     },
-    y: {
-      formatter: (val: number) => `${val} logs`,
+    tooltip: {
+      enabled: true,
+      x: {
+        formatter: (dayOfMonth: number) => `${monthName} ${dayOfMonth}`,
+      },
+      y: {
+        formatter: (val: number) => `${val} logs`,
+      },
     },
-  },
-  grid: {
-    borderColor: colors.bgGrey,
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  // adjustments to chart for smaller screens (ipad, iphone sizes)
-  responsive: [
-    {
-      breakpoint: 1024,
-      options: {
-        xaxis: {
-          labels: {
-            rotate: -45,
-            style: {
-              fontSize: "10px",
+    grid: {
+      borderColor: colors.bgGrey,
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    // adjustments to chart for smaller screens (ipad, iphone sizes)
+    responsive: [
+      {
+        breakpoint: 1024,
+        options: {
+          xaxis: {
+            labels: {
+              rotate: -45,
+              style: {
+                fontSize: "10px",
+              },
             },
           },
         },
       },
-    },
-    {
-      breakpoint: 768,
-      options: {
-        xaxis: {
-          labels: {
-            rotate: -45,
-            style: {
-              fontSize: "10px",
+      {
+        breakpoint: 768,
+        options: {
+          xaxis: {
+            labels: {
+              rotate: -45,
+              style: {
+                fontSize: "10px",
+              },
             },
           },
         },
       },
-    },
-    {
-      breakpoint: 480,
-      options: {
-        xaxis: {
-          labels: {
-            rotate: -90,
-            style: {
-              fontSize: "8px",
+      {
+        breakpoint: 480,
+        options: {
+          xaxis: {
+            labels: {
+              rotate: -90,
+              style: {
+                fontSize: "8px",
+              },
             },
           },
         },
       },
-    },
-  ],
-});
+    ],
+  };
+};
 
 // Functional component to render the line chart for habits logged in a month
 export function LineChart({
@@ -163,6 +170,9 @@ export function LineChart({
   const logsForHabit = habitsLog[selectedHabit]?.[year]?.[month] || {};
   const daysInMonth = getDaysInMonth(year, new Date().getMonth());
 
+  // Convert month name to zero-based month index
+  const monthIndex = new Date(`${month} 1, ${year}`).getMonth();
+
   // Create an array to store the number of logs for each day of the month
   const seriesData = Array.from({ length: daysInMonth }, (_, dayIndex) => {
     const dayOfMonth = dayIndex + 1;
@@ -171,7 +181,7 @@ export function LineChart({
   });
 
   // Get the options object for ApexCharts
-  const options = generateChartOptions(daysInMonth, month);
+  const options = generateChartOptions(daysInMonth, year, monthIndex);
 
   const series = [
     {
