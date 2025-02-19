df0eb9460c37aec5b74759b28d6833bee662a749
Jeffrey Patey <jeffreyvpatey@gmail.com>
2025-02-19 10:03:27 -0400

    updated comments

--- src/config/authApiConfig.ts
+++ src/config/authApiConfig.ts
@@ -1,6 +1,6 @@
 export const API_BASE_URL = "http://localhost:3000";
 
-// API Endpoints
+// Authentication Endpoints
 export const REGISTER_ENDPOINT = `${API_BASE_URL}/auth/register`;
 export const LOGIN_ENDPOINT = `${API_BASE_URL}/auth/login`;
 export const GUEST_LOGIN_ENDPOINT = `${API_BASE_URL}/auth/guest-login`;
--- src/config/quesApiConfig.ts
+++ src/config/quesApiConfig.ts
@@ -1,7 +1,5 @@
 export const API_BASE_URL = "http://localhost:3000";
 
-// Questionnaire endpoint
+// Questionnaire endpoints
 export const QUESTIONNAIRE_RESPONSE_ENDPOINT = `${API_BASE_URL}/questionnaire/response`;
-
-// Questionnaire progress endpoint
 export const QUESTIONNAIRE_PROGRESS_ENDPOINT = `${API_BASE_URL}/questionnaire/progress`;
